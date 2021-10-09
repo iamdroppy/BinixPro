372fe4fb7c0a8b963f524528062d6f896dad66ba
lucca <habboclub97@gmail.com>
2021-10-09 11:42:53 -0300

    init

---
+++ Binix.Api.Model/Zone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Binix.Api.Model
+{
+    public class Zone : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public virtual ICollection<Cluster> Clusters { get; set; }
+    }
+}
